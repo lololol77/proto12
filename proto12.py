5c82f99dcdba1fcc34ee4dcde87ea1950da8a462
lololol77 <2310525@dtg.hs.kr>
2025-03-30 23:49:52 +0900

    Update proto12.py

--- proto12.py
+++ proto12.py
@@ -63,10 +63,12 @@ elif user_type == '지원자':
         for company, job_name, score in results:
             st.write(f'회사: {company}, 업무: {job_name}, 적합도 점수: {score}')
     
+# 유료 서비스 확인
 if st.button('추가 질문'):
     if user_type == '회사':
-        st.session_state['response'] = st.radio('유료 직무개발 서비스 이용하시겠습니까?', ['예', '아니오'], index=1 if st.session_state['response'] == '아니오' else 0)
+        st.session_state['response'] = st.radio('유료 직무개발 서비스 이용하시겠습니까?', ['예', '아니오'], index=0)
     elif user_type == '지원자':
-        st.session_state['response'] = st.radio('유료 취업확인 서비스 이용하시겠습니까?', ['예', '아니오'], index=1 if st.session_state['response'] == '아니오' else 0)
+        st.session_state['response'] = st.radio('유료 취업확인 서비스 이용하시겠습니까?', ['예', '아니오'], index=0)
     if st.button('홈으로 돌아가기'):
         st.experimental_rerun()
+
